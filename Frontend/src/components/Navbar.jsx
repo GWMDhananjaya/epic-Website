d7f3230b52a5264bbf1a8ba6ab29975998e1f15c
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-08-24 02:02:36 +0530

    parts of navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React , { useEffect , useState} from 'react'
 import logo from '../assets/logo.png'
+import { Link } from 'react-scroll';
 
 function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -7,7 +8,7 @@ function Navbar() {
 
     // set toggle Menu
     const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+        setIsMenuOpen(!isMenuOpen);}
 
     useEffect(() => {
         const handleScroll = () => {
@@ -33,13 +34,33 @@ function Navbar() {
         {link: "shop", path: "shop"},
     ]
   return (
-    <nav className="">
-        
-       
+    <header className='fixed top-0 left-0 right-0 w-full bg-black md:bg-transparent'>
+        <nav>
+            <div>
+                <a href="" className='space-x-3 text-2xl item-center bg-slate-400'>
+                    <img src={logo} className='inline-block w-32 item-center'>
+                    </img>
+                </a>
 
-    </nav>
+                <ul className='hidden space-x-12 md:flex'>
+                    {
+                        navItems.map(({link, path}) => <Link key={path} spy={true} smooth={true} to={path} offset={-100} 
+                        className='block text-base text-gray900 hover:text-brandPrimary first:font-medium'>{link}</Link>)
+                    }
+                    
+                </ul>
+                
+                <div className='items-center hidden space-x-12 lg:flex'>
+                    <a href='/' className='hidden lg:flex item-center text-brandPrimary hover:text-gray900'>Login</a>
+                    <button className='px-4 py-2 text-white transition-all duration-300 rounded bg-brandPrimary hover:bg-neutralDGrey'>Sign up</button>
+                </div>
+                
+            </div>
+            
+        </nav>
+    </header>
   )
+
 }
 
 export default Navbar
-
