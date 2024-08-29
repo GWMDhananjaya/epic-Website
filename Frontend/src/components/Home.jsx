b4d4549dd302bf6a9654ddf1d7464ff3776277da
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-08-29 11:08:39 +0530

    update home page

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -4,24 +4,26 @@ import firstimage from "../assets/fimage.png";
 
 function Home() {
   return (
-    <div className="bg-neutralSilver">
-      <div className="h-screen min-h-screen px-4 mx-auto lg:px-14 max-w-screen-2xl">
+    
+    <div className="">
+      <div className="h-screen min-h-screen px-4 mx-auto ">
         <Carousel className="w-full mx-auto">
-          <div className="flex flex-col justify-between gap-12 py-12 my-24 md:my-8-col md:flex-row-reverse item-center">
-            <div>
-              <img src={firstimage} alt="" />
-            </div>
-            
-            <div className="md:w-1/2">
-                <h1 className="mb-4 text-4xl font-semibold leading-snug text-neutralDGrey md:w-3/4">
-                ULTIMATE CAR DETAILING TEAM
-                    <span className="leading-snug text-brandPrimary"> epic, Owner - Nuwan Sampath
-                    </span>
-                </h1>
-                <p className="mb-8 text-base text-neutralGrey"></p>
-                <button className="btn-primary"> Register</button>
-            </div>
-          </div>
+        <div>
+        <div>
+          <h1>
+          ULTIMATE CAR DETAILING TEAM
+          </h1>
+          <p>
+          We offer top-notch vinyl wrapping, window tinting, car modifications, 
+          detailing, body stickers, and more. Get expert care and personalized 
+          vehicle customization all in one place!
+          </p>
+        </div>
+        
+        <div>
+          <img src={firstimage} alt="" />
+        </div>
+      </div>
         </Carousel>
       </div>
     </div>
